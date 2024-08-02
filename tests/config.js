04c89f0fea1b5b0877376ca1b3e9db23f78d0454
Daniel Hartley <daniel.hartley@altruistiq.com>
2024-08-02 15:40:44 +0100

    puppeteer and simple example

---
+++ tests/config.js
@@ -0,0 +1,14 @@
+export const config = {
+  viewport: {
+    desktop: {
+      width: 1920,
+      height: 1080,
+      scrolled: 30000,
+    },
+    mobile: {
+      width: 1080,
+      height: 2400,
+      scrolled: 30000,
+    },
+  },
+}
