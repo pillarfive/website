64ab7cadd925a51026a4878948359139720d24c6
Daniel Hartley <daniel.hartley@altruistiq.com>
2024-08-22 11:29:49 +0100

    configure jest to run unti and e2e tests

--- tests/puppeteer/index.test.js
+++ tests/puppeteer/index.test.js
@@ -1,18 +1,12 @@
-import puppeteer from 'puppeteer'
-import { config } from '/src/tools/config.js'
-;(async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
+/* eslint-disable no-undef */
+import 'expect-puppeteer'
+
+describe('Google', () => {
+  beforeAll(async () => {
+    await page.goto('https://google.com')
   })
 
-  const page = await browser.newPage()
-
-  await page.goto('http://127.0.0.1:1234/')
-
-  await page.setViewport({
-    width: config.viewport.desktop.width,
-    height: config.viewport.desktop.height,
+  it('should be titled "Google"', async () => {
+    await expect(page.title()).resolves.toMatch('Google')
   })
-
-  await browser.close()
-})()
+})
