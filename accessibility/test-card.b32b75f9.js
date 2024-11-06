50d7439f005bc572f9a9cae83f04bc2d0d79c9a3
Daniel Hartley <daniel.hartley@altruistiq.com>
2024-11-06 15:54:43 +0000

    Updates

---
+++ accessibility/test-card.b32b75f9.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",()=>{let e=document.getElementById("go-to-selected-page-btn"),t=Array.from(document.getElementById("how-to-options").querySelectorAll("option"));e.addEventListener("click",()=>{let e=t.find(e=>e.selected).value;document.location.href=e})});
+//# sourceMappingURL=test-card.b32b75f9.js.map
